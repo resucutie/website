230751adb73e46ce54bdbc938ae3f14e026dda27
abUwUser <pedro.santos.cartaxo@gmail.com>
2023-07-01 14:52:07 -0300

    change background, change main color, fix scrolling issues, fix issues related to portrait mode

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,9 +2,30 @@ import styles from "./Projects.module.scss"
 
 export default () => {
     return <div id={styles["projects"]}>
+        <div><h1>Projects</h1></div>
+            <Card title="Quevi"
+                description="Quevi is an decentralized app focused on providing an uninterrupted and accessible studying process by randomly providing questions from existing exams (e.g. PAS, ENEM, etc.)"
+                buttonLink="https://github.com/Quevi-org"
+                buttonText="Check out on GitHub"
+            />
+            <Card title="hospitalurgency.com"
+                description="hospitalurgency.com is a website that shows the nearest hospitals close to your area that have the shortest queue. Currently it is just a concept, and all of the data were mocked up. Maruska Tatiana Nascimento Silva Bueno, CEUB's computer engineering coordinator, has complimented this project. Made for a schoolar project."
+                buttonLink="http://hospitalurgency.auser.catvibers.me"
+                buttonText="Open the website"
+            />
+            <Card title="Culture"
+                description="Culture is a website that shows cultural attractions near your location without the influence of an algorithm. Made for a schoolar project."
+                buttonLink="http://culutre.auser.catvibers.me"
+                buttonText="Open the website"
+            />
+            <Card title="Examer"
+                description="Examer is a CLI application for exam evaluation. Made for a schoolar project."
+                buttonLink="https://github.com/abUwUser/Examer"
+                buttonText="Check out on GitHub"
+            />
         <div className={styles["scroll"]}>
-            <div><h1>Projects</h1></div>
-            <div className="card">
+            
+            {/* <div className="card">
                 <h2>Quevi</h2>
                 <p>
                     Quevi is an decentralized app focused on providing an uninterrupted and accessible studying process by randomly providing questions from existing exams (e.g. PAS, ENEM, etc.)
@@ -31,8 +52,26 @@ export default () => {
                     Examer is a CLI application for exam evaluation. Made for a schoolar project.
                 </p>
                 <BigLinkButton href="https://github.com/abUwUser/Examer">Check out on GitHub</BigLinkButton>
-            </div>
+            </div> */}
         </div>
+    </div>
+}
+
+interface CardProps {
+    title: string
+    description: string
+    buttonLink: string
+    buttonText?: string
+}
+export const Card = (props: CardProps) => {
+    return <div className="card">
+        <div className={styles["content"]}>
+            <h2>{props.title}</h2>
+            <p> {props.description}
+                Quevi is an decentralized app focused on providing an uninterrupted and accessible studying process by randomly providing questions from existing exams (e.g. PAS, ENEM, etc.)
+            </p>
+        </div>
+        <BigLinkButton href={props.buttonLink}>{props.buttonText || "Open link"}</BigLinkButton>
     </div>
 }
 
