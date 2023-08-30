3d6a80ac2c82195a66fc2033359032712c145093
abUwUser <pedro.santos.cartaxo@gmail.com>
2023-08-29 21:13:00 -0300

    link to my art place

--- src/pages/OC.tsx
+++ src/pages/OC.tsx
@@ -2,7 +2,8 @@ import styles from "./About.module.scss"
 
 export default () => {
     return <div id={styles["about"]}>
+        <a className={styles["checkMoreArts"]} href="https://cloud.uratania.ch/s/dnjxAZrwCd2BoE8">Check out for more arts</a>
         <img className={styles["ref"]} src="/ref.png" />
-        <p>Thanks to <a href="https://twitter.com/dishflowers">@dishflowers@twitter.com</a> for doing it!</p>
+        {/* <p>Thanks to <a href="https://twitter.com/dishflowers">@dishflowers@twitter.com</a> for doing it!</p> */}
     </div>
 }
