f55604eaac9882949bdef20bc81f3b9f83e3cdc3
abUwUser <pedro.santos.cartaxo@gmail.com>
2023-10-12 10:51:44 -0300

    oops

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -11,7 +11,7 @@ export default () => {
             <h3>Active</h3>
             <LinkIcon icon={FaGithub} href="https://github.com/abUwUser">abuwuser</LinkIcon>
             <LinkIcon icon={FaDiscord} href="https://discord.com/users/264062457448759296">@resucutie</LinkIcon>
-            <LinkIcon icon={SiFirefish} href="https://furry.engineer/@resu">@resu@kitsunes.club</LinkIcon>
+            <LinkIcon icon={SiFirefish} href="https://kitsunes.club/@resu">@resu@kitsunes.club</LinkIcon>
             <LinkIcon icon={MdEmail} href="mailto:resucutie@proton.me">Email</LinkIcon>
             <LinkIcon icon={SiEpicgames}>abUwUser</LinkIcon>
             <LinkIcon icon={SiMatrix} href="https://matrix.to/#/@abuwuser:catvibers.me">@abuwuser:catvibers.me</LinkIcon>
